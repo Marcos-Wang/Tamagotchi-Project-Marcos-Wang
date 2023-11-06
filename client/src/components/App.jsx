e79c62fcbcfce16772c791cc1269f3bceb484ac9
Marcos Wang <wangfmarcos@gmail.com>
2023-11-06 11:35:29 -0800

    added sprites and components, edited button and header elements.

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,6 +1,11 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import danuIdle from '../assets/DanuIdle.png'
+import '../styles/App.css'
+
+import Title from './Title.jsx'
+// import Monitor from './Monitor.jsx'
+// import Response from './Response.jsx'
+// import Sidebar from './Sidebar.jsx'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,21 +14,15 @@ function App() {
     <>
       <div>
         <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+          <img src={danuIdle} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <Title />
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
---
+++ client/src/components/Title.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from '../assets/react.svg'
+import '../styles/Title.css'
+
+function Title() {
+
+  return (
+    <>
+      <h1>Tamagotchi!</h1>
+    </>
+  )
+}
+
+export default Title
